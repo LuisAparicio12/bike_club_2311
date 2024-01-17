11149dc4e103a2509d3430c7b6f636367d24a326
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-17 02:05:09 -0500

    Create the biker class

--- lib/biker.rb
+++ lib/biker.rb
@@ -6,4 +6,21 @@ class Biker
         @rides = {}
         @acceptable_terrain = []
     end
+
+    def learn_terrain!(terrain)
+        @acceptable_terrain << terrain
+    end
+
+    def log_ride(ride, time)
+        if @acceptable_terrain.include?(ride.terrain) && ride.distance <= @max_distance
+            puts "Cannot log ride: Terrain or distance not correct."
+        else
+            @rides[ride] ||= []
+            @rides[ride] << time
+        end
+    end 
+
+    def personal_record(ride)
+        @rides[ride]&.min || false
+    end 
 end
