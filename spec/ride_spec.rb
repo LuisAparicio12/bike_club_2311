1bfb0fe8d8491e3c1805d599c062228953b267c1
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-16 23:31:03 -0500

    Test for another ride

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Ride do
         expect(ride1).to be_an_instance_of Ride
     end
 
+    it 'can have a total distance' do
+        ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+
+        expect(ride1.loop?).to eq(false)
+        expect(ride1.total_distance).to eq(21.4)
+    end
+
+    it 'can have another ride' do
+        ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+        expect(ride2.loop?).to eq(true)
+        expect(ride2.total_distance).to eq(14.9)
+    end
+
 end
