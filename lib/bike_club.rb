b942e066272e42128b2f7317f4002d2aed7e6530
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-17 02:06:32 -0500

    Create the bike club class

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,19 @@
 class BikeClub
+    attr_reader :name, :bikers
+    def initialize(name)
+        @name = name
+        @bikers = []
+    end
 
+    def add_biker(biker)
+        @bikers << biker
+    end
+
+    def most_rides
+        @bikers.max_by {|biker| biker.rides.count}
+    end
+
+    def best_time(ride)
+        @bikers.min_by {|biker| biker.time_ride(ride)}
+    end
 end
