26264ea3462096f0f82dcf462344505ac0cd9f6e
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-16 23:14:45 -0500

    Create the initialize

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,9 @@
 class Ride
-
+    attr_reader :name, :distance, :loop, :terrain
+    def initialize(hash)
+        @name = hash[:name]
+        @distance = hash[:distance]
+        @loop = hash[:loop]
+        @terrain = [:terrain]
+    end
 end
